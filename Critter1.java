0ac41e7abd9aab041446a0eb862f27f12d8e7a6e
sagar-kris <sagar.krishnaraj@gmail.com>
2016-10-17 12:23:21 -0500

    Critter 1 & 2 .java & .class files

---
+++ Critter1.java
@@ -0,0 +1,82 @@
+package assignment4;
+
+/*
+ * This critter is a basic critter that models the motion patterns
+ * of a knight on a chess board. Since he is a lone soldier, he only
+ * sires one child for the entire duration of his lifetime. He also
+ * has a high amount of health to begin with.
+ */
+public class Critter1 extends Critter{
+	
+	@Override
+	public String toString() { return "1"; }
+	
+	private int dir;
+	
+	public Critter1() {
+		dir = Critter.getRandomInt(16);
+	}
+	
+	public boolean fight(String not_used) {
+		return true;
+	}
+
+	@Override
+	public void doTimeStep() {
+		/* move like a chess knight */
+		jump(dir);
+	}
+	
+	private void jump (int dir) {
+		switch (dir) {
+			case 0: super.run(dir/2);
+					super.walk(dir/2+6);
+					break;
+			case 1: super.run(dir/2);
+					super.walk(dir/2+2);
+					break;
+			case 2: super.run(dir/2);
+					super.walk(dir/2+6);
+					break;
+			case 3: super.run(dir/2);
+					super.walk(dir/2+2);
+					break;
+			case 4: super.run(dir/2);
+					super.walk(dir/2+6);
+					break;
+			case 5: super.run(dir/2);
+					super.walk(dir/2+2);
+					break;
+			case 6: super.run(dir/2);
+					super.walk(dir/2+6);
+					break;
+			case 7: super.run(dir/2);
+					super.walk(dir/2+2);
+					break;
+			case 8: super.run(dir/2);
+					super.walk(dir/2+6);
+					break;
+			case 9: super.run(dir/2);
+					super.walk(dir/2+2);
+					break;
+			case 10: super.run(dir/2);
+					super.walk(dir/2+6);
+					break;
+			case 11: super.run(dir/2);
+					super.walk(dir/2+2);
+					break;
+			case 12: super.run(dir/2);
+					super.walk(dir/2+2);
+					break;
+			case 13: super.run(dir/2);
+					super.walk(dir/2+6);
+					break;
+			case 14: super.run(dir/2);
+					super.walk(dir/2+2);
+					break;
+			case 15: super.run(dir/2);
+					super.walk(dir/2+6);
+					break;
+		}
+	}
+}
